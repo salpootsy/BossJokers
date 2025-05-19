36636b443e3d4f4b9236c9e3ed3516bbd1549d06
BuildTools <unconfigured@null.spigotmc.org>
2025-05-19 13:43:57 +0300

    changed how boss tag works again

--- objects/Tags/BossTag.lua
+++ objects/Tags/BossTag.lua
@@ -9,7 +9,7 @@ SMODS.Tag({
     key = "boss_tag",
     atlas = "boss_tag",
     pos = {x = 0, y = 0},
-    no_collection = true,
+    --no_collection = true,
     discovered = true,
 	config = {type = 'new_blind_choice', extra = {last = false}},
 	loc_vars = function(self, info_queue, card)
@@ -19,10 +19,8 @@ SMODS.Tag({
 		return MP.LOBBY.code and MP.LOBBY.config.multiplayer_jokers and enemy_has_boss_jokers()
 	end,
     apply = function (self, tag, context)
-        if context.type == 'tag_add' and context.tag.key == self.key then
-            tag:yep('-', G.C.RED, function ()
-                return true
-            end)
+        if context.type == 'tag_add' and context.tag.key == self.key and tag ~= nil then
+            tag:remove()
         end
         if context.type == self.config.type and have_boss_phantom() then
             tag:yep('+', G.C.GREEN,function()
